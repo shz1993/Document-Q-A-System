ac62b551c4f5a26a47a64e8ea279e4dea2d38e55
shz1993 <yoonvin99@gmail.com>
2026-06-11 21:26:14 +0700

    Fix import: use langchain_classic.chains

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS  # Ganti Chroma dengan FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
+from langchain_classic.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 
 # Utils
